3e08dc2eaf284316082be4ca8afeb67421806806
Mark Halls <mark.halls@gmail.com>
2019-12-19 16:10:23 -0700

    Post /user returns the added user object

--- api/server.js
+++ api/server.js
@@ -1,6 +1,15 @@
 const express = require(`express`);
 
+const Users = require(`../data/users/users-model`);
+
 const server = express();
+server.use(express.json());
 server.get(`/`, (req, res) => res.status(200).json({ msg: `api is up` }));
 
+server.post(`/user`, (req, res) => {
+  const user = req.body;
+  Users.add(user).then(([id]) =>
+    Users.find(id).then(user => res.status(200).json(user))
+  );
+});
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,8 +1,12 @@
 const request = require(`supertest`);
-
+const db = require(`../data/dbConfig`);
 const server = require(`./server`);
 
 describe(`server.js`, () => {
+  beforeEach(async () => {
+    await db(`users`).truncate();
+  });
+
   describe(`get /`, () => {
     it(`should return a 200 OK`, () => {
       return request(server)
@@ -14,7 +18,17 @@ describe(`server.js`, () => {
     it(`should return {mgs: "api is up"}`, () => {
       return request(server)
         .get(`/`)
-        .then(res => expect(res.body).toBe({ msg: "api is up" }));
+        .then(res => expect(res.body).toEqual({ msg: "api is up" }));
+    });
+  });
+
+  describe(`post /user`, () => {
+    it(`should return added user object`, () => {
+      const user = { username: "bilbo", password: "baggins" };
+      return request(server)
+        .post(`/user`)
+        .send(user)
+        .then(res => expect(res.body).toMatchObject(user));
     });
   });
 });
